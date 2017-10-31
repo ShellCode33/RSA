4b0dbceb1221f15ca8ff5f1e743bb62c05d31110
ShellCode33 <clement.fleury33@gmail.com>
2017-10-31 17:28:22 +0100

    Fix everything

--- src/rsa/utils/shellcode/Main.java
+++ src/rsa/utils/shellcode/Main.java
@@ -1,23 +1,33 @@
 package rsa.utils.shellcode;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
-
 /**
- * Created by shellcode on 2/10/17.
+ * Created by shellcode on 10/30/17.
  */
 public class Main {
     public static void main(String [] args) {
 
-        RSA rsa = new RSA(2048);
+        RSA rsa = new RSA(4096);
         rsa.generateKeys();
 
-        String msg = "Bonsoir comment allez-vous ?";
+        String msg = "RSA (Rivest–Shamir–Adleman) is one of the first practical public-key cryptosystems and is widely used for secure data transmission. In such a cryptosystem, the encryption key is public and it is different from the decryption key which is kept secret (private). In RSA, this asymmetry is based on the practical difficulty of the factorization of the product of two large prime numbers, the \"factoring problem\". The acronym RSA is made of the initial letters of the surnames of Ron Rivest, Adi Shamir, and Leonard Adleman, who first publicly described the algorithm in 1978. Clifford Cocks, an English mathematician working for the British intelligence agency Government Communications Headquarters (GCHQ), had developed an equivalent system in 1973, but this was not declassified until 1997. A user of RSA creates and then publishes a public key based on two large prime numbers, along with an auxiliary value. The prime numbers must be kept secret. Anyone can use the public key to encrypt a message, but with currently published methods, and if the public key is large enough, only someone with knowledge of the prime numbers can decode the message feasibly. Breaking RSA encryption is known as the RSA problem. Whether it is as difficult as the factoring problem remains an open question. RSA is a relatively slow algorithm, and because of this, it is less commonly used to directly encrypt user data. More often, RSA passes encrypted shared keys for symmetric key cryptography which in turn can perform bulk encryption-decryption operations at much higher speed.";
+
+        System.out.println("Encrypting string which is " + msg.length() + " characters long.");
+
         String cipher_b64 = rsa.encode(msg, rsa.getPublicKey(), rsa.getMod());
         System.out.println("cipher : " + cipher_b64);
-        System.out.println("plaintext : " + rsa.decode(cipher_b64, rsa.getPrivateKey(), rsa.getMod()));
+        String decrypted_msg = rsa.decodeToString(cipher_b64, rsa.getPrivateKey(), rsa.getMod());
+        System.out.println("plaintext : " + decrypted_msg);
+
+        if(msg.equals(decrypted_msg)) {
+            System.out.println("RSA is working !!");
+        }
+
+        else {
+            System.out.println("Oops, something went wrong !");
+        }
 
         System.out.println("\n\n" + rsa.getFormatedPublicKey());
         System.out.println("\n\n" + rsa.getFormatedPrivateKey());
+
     }
 }
--- src/rsa/utils/shellcode/RSA.java
+++ src/rsa/utils/shellcode/RSA.java
@@ -16,52 +16,54 @@ import java.util.Arrays;
 import java.util.Base64;
 
 /**
- * Created by shellcode on 2/10/17.
+ * Created by shellcode on 10/30/17.
  */
-public class RSA {
+class RSA {
 
-    int mKeySize;
-    BigInteger mPublicKey = null;
-    BigInteger mPrivateKey = null;
-    BigInteger mMod = null;
+    private int mKeySize;
+    private BigInteger mPublicKey = null;
+    private BigInteger mPrivateKey = null;
+    private BigInteger mMod = null;
 
 
-    public RSA(int key_size) {
+    RSA(int key_size) {
         if (key_size != 512 && key_size != 1024 && key_size != 2048 && key_size != 4096)
             throw new IllegalArgumentException("Supported key size : 512, 1024, 2048, 4096");
 
         mKeySize = key_size;
     }
 
-    public void generateKeys() {
+    void generateKeys() {
 
-        System.out.println("Generating keys...");
+        System.out.println("Generating " + mKeySize + " bits keys...");
 
         SecureRandom random = new SecureRandom();
-        BigInteger p = new BigInteger(mKeySize / 2 - 1, 100, random);
-        BigInteger q = new BigInteger(mKeySize / 2 - 1, 100, random);
+
+        //On divise la taille voulue de la clé par 2 afin d'avoir un n qui sera sur le nb de bits voulu.
+        BigInteger p = new BigInteger(mKeySize / 2, 100, random);
+        BigInteger q = new BigInteger(mKeySize / 2, 100, random);
+
         mMod = p.multiply(q);
 
-        BigInteger phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        BigInteger phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)); // phi(n) = (p-1)(q-1)
 
         BigInteger e;
 
         do {
-            e = new BigInteger(mKeySize - 1, random);
-        } while (e.compareTo(BigInteger.ONE) != 1 || e.compareTo(phi_n) != -1 || !e.gcd(phi_n).equals(BigInteger.ONE));
+            e = new BigInteger(mKeySize - 1, random);   //On génère une exposant aléatoire
+        } while (!e.gcd(phi_n).equals(BigInteger.ONE)); //Tant que le nombre n'est pas premier avec phi_n, on en génère un nouveau
 
-        BigInteger d = e.modInverse(phi_n);
-
-//        System.out.println("p = " + p.toString() + "\n");
-//        System.out.println("q = " + q.toString() + "\n");
-//        System.out.println("phi(n) = " + phi_n.toString() + "\n");
-//        System.out.println("e = " + e.toString() + "\n");
-//        System.out.println("d = " + d.toString() + "\n");
-
+        BigInteger d = e.modInverse(phi_n); //Notre clé privée est le coupe (e^-1[phi_n], mMOd)
+/*
+        System.out.println("p = " + p.toString() + "\n");
+        System.out.println("q = " + q.toString() + "\n");
+        System.out.println("phi(n) = " + phi_n.toString() + "\n");
+        System.out.println("e = " + e.toString() + "\n");
+        System.out.println("d = " + d.toString() + "\n");
         System.out.println("private_key size : " + d.bitLength());
         System.out.println("public_key size : " + e.bitLength());
         System.out.println("mod size : " + mMod.bitLength());
-
+*/
         mPublicKey = e;
         mPrivateKey = d;
 
@@ -69,27 +71,27 @@ public class RSA {
 
     }
 
-    public BigInteger getPrivateKey() {
+    BigInteger getPrivateKey() {
         return mPrivateKey;
     }
 
-    public void setPublicKey(String b64PublicKey) {
+    void setPublicKey(String b64PublicKey) {
         mPublicKey = new BigInteger(Base64.getDecoder().decode(b64PublicKey));
     }
 
-    public void setPrivateKey(String b64PrivateKey) {
+    void setPrivateKey(String b64PrivateKey) {
         mPrivateKey = new BigInteger(Base64.getDecoder().decode(b64PrivateKey));
     }
 
-    public void setKeySize(int size) {
+    void setKeySize(int size) {
         mKeySize = size;
     }
 
-    public void setModulus(String b64Modulus) {
+    void setModulus(String b64Modulus) {
         mMod = new BigInteger(Base64.getDecoder().decode(b64Modulus));
     }
 
-    public String getFormatedPrivateKey() {
+    String getFormatedPrivateKey() {
 
         return "<PrivateKey>\n" +
                 "    <Exponent>" +
@@ -102,11 +104,11 @@ public class RSA {
                 "</PrivateKey>";
     }
 
-    public BigInteger getPublicKey() {
+    BigInteger getPublicKey() {
         return mPublicKey;
     }
 
-    public String getFormatedPublicKey() {
+    String getFormatedPublicKey() {
         return "<PublicKey>\n" +
                 "    <Exponent>" +
                 Base64.getEncoder().encodeToString(mPublicKey.toByteArray()) +
@@ -118,11 +120,11 @@ public class RSA {
                 "</PublicKey>";
     }
 
-    public void savePrivateKeyToFile(String path) {
+    void savePrivateKeyToFile(String path) {
         saveToFile(path, getFormatedPrivateKey());
     }
 
-    public void savePublicKeyToFile(String path) {
+    void savePublicKeyToFile(String path) {
         saveToFile(path, getFormatedPublicKey());
     }
 
@@ -136,18 +138,18 @@ public class RSA {
         }
     }
 
-    public BigInteger getMod() {
+    BigInteger getMod() {
         return mMod;
     }
 
-    public String encode(String message, BigInteger public_key, BigInteger mod) {
+    String encode(String message, BigInteger public_key, BigInteger mod) {
         if (message == null || public_key == null || mod == null)
             throw new InvalidParameterException("parameters can't be null");
 
         byte msg_bytes[] = null;
 
         try {
-            message = Normalizer.normalize(message, Normalizer.Form.NFD); //fix bugs d'accents
+            message = Normalizer.normalize(message, Normalizer.Form.NFD); //fix bug d'accents
             msg_bytes = message.getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
@@ -157,37 +159,58 @@ public class RSA {
     }
 
     //retourne de la base 64
-    public String encode(byte[] msg_bytes, BigInteger public_key, BigInteger mod) {
+    String encode(byte[] msg_bytes, BigInteger public_key, BigInteger mod) {
 
         if (msg_bytes == null || public_key == null || mod == null)
             throw new InvalidParameterException("parameters can't be null");
 
-        int block_size = mod.bitLength() / 8 - 2; //On définit la taille d'un block en fonction du modulo car  1 < m < n   (avec m = taille message et n = p * q = modulo)
-        int nb_sub_messages = (int) Math.ceil((double) msg_bytes.length / block_size);
+        //On définit la taille d'un block en fonction de la taille de p et de q.
+        //Le message M doit être premier avec le modulo N. Pour s'en assurer, on fait en sorte que le message soit plus petit que p et q,
+        // de cette manière, on est sûrs que M et N seront premiers entre eux (vu que N = p * q avec p et q premiers)
+        float block_size_bits = (float)mKeySize / 2 - 1; //p et q sont sur mKeySize/2 bits, donc on prend la valeur juste en dessous --->  -1
+
+        //Mais il faut que la taille du bloc soit divisible par 8 afin de pouvoir passer de bits à octets (et inversement) sans problème.
+        while((block_size_bits / 8) % 1 != 0)
+            block_size_bits--;
+
+        int block_size = (int)block_size_bits;
+
+        //System.out.println("Un bloc doit faire " + block_size + " bits");
+        //System.out.println("Le message fait " + msg_bytes.length + " octets, donc " + msg_bytes.length*8 + " bits");
+
+        int nb_sub_messages = (int) Math.ceil((float)msg_bytes.length / (block_size / 8));
+        //System.out.println("Il y aura " + nb_sub_messages + " blocs");
 
         byte[][] blocks = new byte[nb_sub_messages][];
 
         int copied_bytes = 0;
         int i = 0;
-        for (; i < nb_sub_messages - 1; i++) {
-            byte[] msg_block = Arrays.copyOfRange(msg_bytes, i * block_size, (i + 1) * block_size);
+        for (; i < nb_sub_messages - 1; i++) { //On chiffre chaque block
+            //System.out.println("Bloc n°" + (i+1));
+
+            byte[] msg_block = Arrays.copyOfRange(msg_bytes, i * block_size / 8, (i + 1) * block_size / 8);
+            //System.out.println("Encode : " + new String(msg_block));
+            //System.out.println("msg_block size : " + msg_block.length);
             copied_bytes += msg_block.length;
 
             BigInteger message_integer = new BigInteger(msg_block);
-            BigInteger cipher = message_integer.modPow(public_key, mod);
+            BigInteger cipher = message_integer.modPow(public_key, mod); //On chiffre le bloc
             byte[] cipher_bytes = cipher.toByteArray();
 
-            byte[] block = new byte[mKeySize / 8];
+            byte[] block = new byte[mKeySize / 8 + 1]; //Parfois le BigInteger chiffré fait, pour une raison totalement inconnue, 1 octet de plus que le modulo. Du coup on fait +1 afin de prévoir ce cas particulier
 
-            int block_index = mKeySize / 8 - 1;
+            int block_index = block.length - 1;
             for (int j = cipher_bytes.length - 1; j >= 0; j--)
                 block[block_index--] = cipher_bytes[j];
 
-            System.out.println("encoded : " + Arrays.toString(block));
+            //System.out.println("encoded : " + Arrays.toString(block));
 
             blocks[i] = block;
         }
 
+        //System.out.println("" + copied_bytes + " octets copiés.");
+
+        //On s'occupe du dernier bloc qui va jusqu'à la fin
         if (msg_bytes.length - copied_bytes > 0) {
             byte[] msg_block = Arrays.copyOfRange(msg_bytes, copied_bytes, msg_bytes.length);
 //            System.out.println("encode block : " + Arrays.toString(msg_block));
@@ -195,16 +218,20 @@ public class RSA {
             BigInteger message_integer = new BigInteger(msg_block);
             BigInteger cipher = message_integer.modPow(public_key, mod);
             byte[] cipher_bytes = cipher.toByteArray();
-            byte[] block = new byte[mKeySize / 8];
 
-            int block_index = mKeySize / 8 - 1;
+            //System.out.println("cipher_bytes : " + Arrays.toString(cipher_bytes));
+
+            //System.out.println("Cipher size : " + cipher_bytes.length + "+1");
+            byte[] block = new byte[mKeySize / 8 + 1]; //Parfois le BigInteger chiffré fait, pour une raison totalement inconnue, 1 octet de plus que le modulo. Du coup on fait +1 afin de prévoir ce cas particulier
+
+            int block_index = block.length - 1;
             for (int j = cipher_bytes.length - 1; j >= 0; j--)
                 block[block_index--] = cipher_bytes[j];
 
             blocks[i] = block;
         }
 
-        byte[] inline_blocks = new byte[nb_sub_messages * mKeySize / 8];
+        byte[] inline_blocks = new byte[nb_sub_messages * (mKeySize / 8 + 1)];
 
         int index = 0;
         for (byte[] block : blocks) {
@@ -220,7 +247,7 @@ public class RSA {
         }
     }
 
-    public String decodeToString(String b64, BigInteger private_key, BigInteger mod) {
+    String decodeToString(String b64, BigInteger private_key, BigInteger mod) {
         try {
             return new String(decode(b64, private_key, mod), "UTF-8");
         } catch (UnsupportedEncodingException e) {
@@ -229,7 +256,7 @@ public class RSA {
         }
     }
 
-    public byte[] decode(String b64, BigInteger private_key, BigInteger mod) {
+    byte[] decode(String b64, BigInteger private_key, BigInteger mod) {
 
         if (b64 == null || private_key == null || mod == null)
             throw new InvalidParameterException("parameters can't be null");
@@ -242,7 +269,8 @@ public class RSA {
             return null;
         }
 
-        int nb_blocks = (cipher.length * 8) / mKeySize;
+        int nb_blocks = cipher.length / (mKeySize/8+1); //Le fameux +1 pour résoudre le bug étrange
+        //System.out.println("Il y a " + nb_blocks + " blocs");
 
         byte[][] blocks = new byte[nb_blocks][];
         int total_blocks_size = 0;
@@ -250,26 +278,14 @@ public class RSA {
         int index = 0;
         for (int i = 0; i < nb_blocks; i++) {
 
-            byte[] cipher_bytes = Arrays.copyOfRange(cipher, i * mKeySize / 8, (i + 1) * mKeySize / 8);
+            byte[] cipher_bytes = Arrays.copyOfRange(cipher, i * (mKeySize / 8 + 1), (i + 1) * (mKeySize / 8 + 1));//Le fameux +1 pour résoudre le bug étrange
+
+            //System.out.println("[DECRYPT] Block size : " + cipher_bytes.length);
+
             byte[] decrypted_bytes = new BigInteger(cipher_bytes).modPow(private_key, mod).toByteArray();
 
-            System.out.println("decrypted_bytes size : " + decrypted_bytes.length);
-
-            int padding_size = 0;
-            while (decrypted_bytes[padding_size] == 0)
-                padding_size++;
-
-            if (padding_size > 0) {
-                System.out.println("padding detected ! Removing it ;)");
-
-                byte[] removed_padding = new byte[decrypted_bytes.length - padding_size];
-
-                int remove_padding_index = 0;
-                for (int j = padding_size; j < decrypted_bytes.length; j++)
-                    removed_padding[remove_padding_index++] = decrypted_bytes[j];
-
-                decrypted_bytes = removed_padding;
-            }
+            //.out.println("decrypted_bytes size : " + decrypted_bytes.length);
+            //System.out.println("decrypted : " + new String(decrypted_bytes));
 
             blocks[index++] = decrypted_bytes;
             total_blocks_size += decrypted_bytes.length;
@@ -286,21 +302,21 @@ public class RSA {
         return inline_blocks;
     }
 
-    public String sign(String b64Cipher, String hashAlgorithm, BigInteger private_key, BigInteger mod) {
+    String sign(String b64Cipher, String hashAlgorithm, BigInteger private_key, BigInteger mod) {
 
         String cipherHash = Base64.getEncoder().encodeToString(hash(Base64.getDecoder().decode(b64Cipher), hashAlgorithm));
         System.out.println("created hash : " + cipherHash);
 
         String result = encode(b64Cipher + ":" + cipherHash, private_key, mod);
 
-        return result; //we use the private key as a public key in order to sign the message
+        return result; //we use the private key as a key in order to sign the message
     }
 
     //retourne de la base 64
-    public String checkSignatureAndReturnUnsigned(String b64Cipher, String hashAlgorithm, BigInteger public_key, BigInteger mod) throws InvalidSignatureValueException {
+    String checkSignatureAndReturnUnsigned(String b64Cipher, String hashAlgorithm, BigInteger public_key, BigInteger mod) throws InvalidSignatureValueException {
 
         System.out.println("checking signature...");
-        String content = decodeToString(b64Cipher, public_key, mod); //we use the public key to unsign
+        String content = decodeToString(b64Cipher, public_key, mod); //we use the key to unsign
         String[] parts = content.split(":");
 
         if (parts.length != 2)
@@ -319,7 +335,7 @@ public class RSA {
         return cipherB64;
     }
 
-    public byte[] hash(byte[] bytes, String algo) {
+    byte[] hash(byte[] bytes, String algo) {
         byte[] hash = null;
 
         try {
@@ -331,11 +347,11 @@ public class RSA {
         return hash;
     }
 
-    public static String[] getSupportedHashAlgorithms() {
+    static String[] getSupportedHashAlgorithms() {
         return new String[]{"MD5", "SHA-1", "SHA-256", "SHA-512"};
     }
 
-    public static Integer[] getSupportedKeySize() {
+    static Integer[] getSupportedKeySize() {
         return new Integer[]{512, 1024, 2048, 4096};
     }
 }
