ee253bde792c5487079a77746fb14c46f6ad33da
ShellCode <ShellCode33@users.noreply.github.com>
2018-03-26 19:08:47 +0200

    Update RSA.java

--- src/rsa/utils/shellcode/RSA.java
+++ src/rsa/utils/shellcode/RSA.java
@@ -167,13 +167,11 @@ class RSA {
         //On définit la taille d'un block en fonction de la taille de p et de q.
         //Le message M doit être premier avec le modulo N. Pour s'en assurer, on fait en sorte que le message soit plus petit que p et q,
         // de cette manière, on est sûrs que M et N seront premiers entre eux (vu que N = p * q avec p et q premiers)
-        float block_size_bits = (float)mKeySize / 2 - 1; //p et q sont sur mKeySize/2 bits, donc on prend la valeur juste en dessous --->  -1
+        int block_size = mKeySize / 2 - 1; //p et q sont sur mKeySize/2 bits, donc on prend la valeur juste en dessous --->  -1
 
         //Mais il faut que la taille du bloc soit divisible par 8 afin de pouvoir passer de bits à octets (et inversement) sans problème.
         while(block_size_bits % 8 != 0)
             block_size_bits--;
-
-        int block_size = (int)block_size_bits;
 
         //System.out.println("Un bloc doit faire " + block_size + " bits");
         //System.out.println("Le message fait " + msg_bytes.length + " octets, donc " + msg_bytes.length*8 + " bits");
